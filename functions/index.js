3de20b7dc4447b67fba7b2304dcd616f9d5bc492
Rostyslav <rhaidukov@amcbridge.com>
2019-09-26 19:18:53 +0300

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,18 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+
+require('dotenv').config();
 require('cors')({origin: true});
 
-const gmailEmail = functions.config().gmail.login;
-const gmailPassword = functions.config().gmail.pass;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.sendEmail = functions.https.onCall((data) => {
+exports.sendEmail = functions.https.onCall(async (data) => {
     const { name, email, phone, message } = data;
-    console.log(data);
-    const MailTransport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: gmailEmail,
-            pass: gmailPassword
-        }
-    });
-
-    return MailTransport.sendMail({
+    const msg = {
+        to: process.env.RECEIVER_EMAIL,
         from: email,
-        to: 'rostyslav.haidukov@live.com',
-        subject: `Cleaning Service ${name} ${phone} <${email}`,
-        html: `<div>${message}</div>`
-    });
+        subject: `Cleaning Service: ${name} - ${email} - ${phone}`,
+        text: message
+    };
+    await sgMail.send(msg);
 });
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const fs = require('fs');
 const host = "127.0.0.1";
 const port = 1337;
 const express = require("express");
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey('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');
+
+const msg = {
+    to: 'rostyk.haidukov1999@gmail.com',
+    from: 'rostyslav.haidukov@live.com',
+    subject: 'Huy',
+    text: 'huy123',
+    html: '<stron>huy suka</strong>'
+};
+
+sgMail.send(msg);
 
 const app = express();
 app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
@@ -12,27 +24,5 @@ app.get("*", function(request, response){ //root dir
     response.sendFile('public/index.html');
 });
 
-const sendEmail = async () => {
-    const MailTransport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'rostyk.haidukov1999@gmail.com',
-            pass: 'Ro$tyslav99'
-        }
-    });
-
-    return await MailTransport.sendMail({
-        from: 'dasd@gmail.com',
-        to: 'rostyslav.haidukov@live.com',
-        subject: `Cleaning Service`,
-        html: `<div>d</div>`
-    });
-};
-
-sendEmail().then(() => {
-    console.log('success');
-}).catch(error => {
-    console.log(error);
-})
 
 app.listen(port, host);
